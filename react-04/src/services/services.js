f782f2996e68eb6640022658c9dacd51b34e694d
simabui <sima_sima@live.ru>
2020-03-28 13:39:45 +0200

    add starter files

---
+++ react-04/src/services/services.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const KEY = '15109703-4df3afa39634f93d9eb19fc69';
+const URL = `https://pixabay.com/api?&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+/* eslint-disable-next-line */
+export const getRequest = (query, page) =>
+  axios.get(`${URL}&q=${query}&page=${page}`);
