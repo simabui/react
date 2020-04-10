e774e9d3602bde3514ab4708f387bddf3393ffa5
simabui <sima_sima@live.ru>
2020-04-10 14:41:45 +0300

    Updates

---
+++ precache-manifest.ea85a3969c0bb09496a111f65e0be687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c8a2dc8f1ae46132248",
+    "url": "/simabui/friendlist/static/css/main.a59fc81c.chunk.css"
+  },
+  {
+    "revision": "3c8a2dc8f1ae46132248",
+    "url": "/simabui/friendlist/static/js/main.e6e39416.chunk.js"
+  },
+  {
+    "revision": "89eba921f74fe2d92df1",
+    "url": "/simabui/friendlist/static/js/runtime~main.9a9427ae.js"
+  },
+  {
+    "revision": "ebc458bb97945375c6c9",
+    "url": "/simabui/friendlist/static/js/2.f9d42e77.chunk.js"
+  },
+  {
+    "revision": "a3a05f262360f8495a8b85a7f4bbc28d",
+    "url": "/simabui/friendlist/index.html"
+  }
+];
