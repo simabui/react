c668f106c292f04410c3c85ecb1975d1b0b107d2
simabui <sima_sima@live.ru>
2020-04-25 19:10:54 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/friendlist/precache-manifest.4e90852984be609087fb113e985839cf.js"
+  "/phonebook/precache-manifest.9f070fe9f48331237a6945c1ed12e89d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/friendlist/index.html", {
+workbox.routing.registerNavigationRoute("/phonebook/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
