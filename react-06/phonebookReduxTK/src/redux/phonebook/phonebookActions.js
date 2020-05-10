e6dd978dfaa014e32bc86aed92bde9543c461318
simabui <sima_sima@live.ru>
2020-05-10 20:17:55 +0300

    edit redux actions

--- react-06/phonebookReduxTK/src/redux/phonebook/phonebookActions.js
+++ react-06/phonebookReduxTK/src/redux/phonebook/phonebookActions.js
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const updateCollection = createAction('COLLECTION_UPDATE');
-export const filterCollection = createAction('FILTER_COLLECTION');
-export const deleteUser = createAction('DELETE_USER');
+export const updateCollection = createAction('phonebook/COLLECTION_UPDATE');
+export const filterCollection = createAction('phonebook/FILTER_COLLECTION');
+export const deleteUser = createAction('phonebook/DELETE_USER');
